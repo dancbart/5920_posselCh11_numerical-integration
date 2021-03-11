fcf12a28da6bb477647355b03bf8a9e0bf50b2d6
dan <daniel@barton-web.com>
2021-03-11 00:14:50 -0800

    upload to git

--- wk5_num-int_v1.py
+++ wk5_num-int_v1.py
@@ -1,17 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+## 11.1 Step-by-step numerical integration: Euler method
+
+#Constants
 k = 0.5
 m = 1.0
 numberOfSteps = 30000
-DeltaT + 0.001
+DeltaT = 0.001
 
+#This is how we parse out time for horizontal axis
 tCollector=np.linspace(0,numberOfSteps*DeltaT, numberOfSteps+1)
 
-# Initial Conditions
+#this is basically a "theoretical" calculation of the solution to the differential equation.
+#It's just a sine curve.  We will plot x_theo + y_theo on the same graph as xCollector + vCollector.
+#If they lay directly on top of eachother, this indicates a nice fit (i.e. we got the mathematics correct, AND we put in enough steps 30,000)
 x_theo = np.arange(np.pi*9.54929658552,DeltaT)
 y_theo = np.cos(((k/m)**.5)*x_theo)
 
+# Initial Conditions for numerical calculations
 xCollector=[1.0]
 vCollector=[0]
 for ii in range(numberOfSteps):
@@ -20,5 +27,17 @@ for ii in range(numberOfSteps):
     xCollector.append(xNew)
     vCollector.append(vNew)
 
-plt.plot(tCollector,xCollector)
-plt.plot(x_theo,y_theo)
+#plt.plot(tCollector,xCollector)
+#plt.plot(x_theo,y_theo)
+
+# 11.2 Numerical errors
+
+diff = xCollector-y_theo
+print (diff)
+
+plt.plot (tCollector,diff)
+
+#fig = plt.figure()
+#ax = fig.add_subplot()
+#ax.set_title('Chap11 possel',pad=20)
+#ax.plot(tCollector,diff)
